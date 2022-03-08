278f31f8b70b2430ffabe0dfdb6175fbd1e47a71
Deepanshu Batra <deepanshu.batra@akalinfosys.com>
2022-03-08 21:31:37 +0530

    introduction to Angular Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'users',component:UsersComponent},
+  {path:'categories',component:CategoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
